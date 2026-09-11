bf45438dd0ed5e00629f3d0fe717e498ab5a7a02
Ashley Almeida <155456903+ashleyalmeida07@users.noreply.github.com>
2026-09-11 08:59:52 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -101,7 +101,7 @@ function Header() {
               tech community.
             </p>
             <a
-              href="/img/Ashley_Almeida_Resume-1.pdf"
+              href="/img/public/Ashley_Almeida_Resume.pdf"
               className="btn"
                target="_blank"
   rel="noopener noreferrer"
